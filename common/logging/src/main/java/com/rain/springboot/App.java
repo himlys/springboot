d0949223a3fcd70bafd06d3d4f90c68b05f94dd7
himlys <himlys@hotmail.com>
2018-12-29 15:08:12 +0800

    初始化

---
+++ common/logging/src/main/java/com/rain/springboot/App.java
@@ -0,0 +1,13 @@
+package com.rain.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
