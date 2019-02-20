9304ff452d7c8750b82266e046c77dcd81f029fd
himlys <himlys@hotmail.com>
2019-02-20 22:56:42 +0800

    提交代码

--- javaConcurrent/src/main/java/com/rain/java/concurrent/cas/CustomFutureTaskThreadPoolExecutor.java
+++ javaConcurrent/src/main/java/com/rain/java/concurrent/cas/CustomFutureTaskThreadPoolExecutor.java
@@ -10,7 +10,7 @@ public class CustomFutureTaskThreadPoolExecutor<T> extends ThreadPoolExecutor {
 //    Worker中有个getTask函数，从Queue中获取消息，其中使用的是take函数，意味着，这里将无线等待下去。所以，我们的程序使用PoolExecutor的时候要谨慎，防止不停的创建该对象。
 //    除非作为一个公用的线程池，系统启动后一直使用，不然的话，尽量在使用完线程池的时候，关闭当前线程池。
 //    建议规划好代码，避免重复创建线程池，尽量使用公用对象，也就是所有请求使用同一个ThreadPoolExecutor
-
+//
 //    submit函数，本质上还是调用execute函数，在调用之前做一个RunnableFuture对象，该对象主要是实现run函数，还有get函数
 //    run函数调用目标程序，get函数获取结果对象，这里在获取的时候先判定目标程序是否执行完成，如果没完成，主线程进入等待，在目标线程完成之后
 //    回调告诉主线程，返回结果。阻塞主线程主要使用的是unsafe.park，新JDK的很多地方都是用的这种处理方式。
